1aaa95348a0644f8448f59469d1708878c042a65
QuadCorexRV <smooshman12345@gmail.com>
2024-05-17 12:35:42 +1000

    Fix Kyori Adventure-Tom incorrect version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -53,7 +53,7 @@ object Versions {
     
     object Bukkit {
         const val minecraft = "1.20.6"
-        const val paperBuild = "$minecraft-R0.1-20240501.172135-12"
+        const val paperBuild = "$minecraft-R0.1-20240516.001739-56"
         const val paper = paperBuild
         const val paperLib = "1.0.8"
         const val reflectionRemapper = "0.1.1"
