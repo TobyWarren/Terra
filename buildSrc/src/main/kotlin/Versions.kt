dba70f92bd8549dab40df767e6dd6e6c80197aba
QuadCorexRV <smooshman12345@gmail.com>
2024-06-19 20:51:13 +1000

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -27,7 +27,7 @@ object Versions {
     }
     
     object Fabric {
-        const val fabricAPI = "0.97.8+${Mod.minecraft}"
+        const val fabricAPI = "0.100.0+${Mod.minecraft}"
     }
 //
 //    object Quilt {
@@ -36,11 +36,11 @@ object Versions {
 //    }
     
     object Mod {
-        const val mixin = "0.12.5+mixin.0.8.5"
+        const val mixin = "0.14.0+mixin.0.8.6"
         
         const val minecraft = "1.20.6"
         const val yarn = "$minecraft+build.1"
-        const val fabricLoader = "0.15.10"
+        const val fabricLoader = "0.15.11"
         
         const val architecuryLoom = "1.6.395"
         const val architecturyPlugin = "3.4.155"
