4ef372a63cf1fddbb97068245f8752f0c525bdbf
QuadCorexRV <smooshman12345@gmail.com>
2024-12-12 11:19:30 +1100

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 dependencies {
     //TODO Allow pulling from Versions.kt
     implementation("com.gradleup.shadow", "shadow-gradle-plugin", "8.3.1")
-    implementation("io.papermc.paperweight.userdev", "io.papermc.paperweight.userdev.gradle.plugin", "1.7.2")
+    implementation("io.papermc.paperweight.userdev", "io.papermc.paperweight.userdev.gradle.plugin", "1.7.4")
 
-    implementation("org.ow2.asm", "asm", "9.7")
+    implementation("org.ow2.asm", "asm", "9.7.1")
     implementation("org.ow2.asm", "asm-tree", "9.7")
     implementation("com.dfsek.tectonic", "common", "4.2.1")
     implementation("org.yaml", "snakeyaml", "2.3")
