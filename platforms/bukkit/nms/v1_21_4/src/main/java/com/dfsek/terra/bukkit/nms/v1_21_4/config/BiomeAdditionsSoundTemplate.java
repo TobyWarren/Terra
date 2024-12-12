509c982943ff2de4711f9717e740eb9d5b9871a4
QuadCorexRV <smooshman12345@gmail.com>
2024-12-12 12:43:55 +1100

    Update BiomeAdditionsSoundTemplate.java

--- platforms/bukkit/nms/v1_21_4/src/main/java/com/dfsek/terra/bukkit/nms/v1_21_4/config/BiomeAdditionsSoundTemplate.java
+++ platforms/bukkit/nms/v1_21_4/src/main/java/com/dfsek/terra/bukkit/nms/v1_21_4/config/BiomeAdditionsSoundTemplate.java
@@ -1,4 +1,4 @@
-package com.dfsek.terra.bukkit.nms.v1_21_3.config;
+package com.dfsek.terra.bukkit.nms.v1_21_4.config;
 
 import com.dfsek.tectonic.api.config.template.annotations.Default;
 import com.dfsek.tectonic.api.config.template.annotations.Value;
