49ef3ccec162748a02807d9c401af66a19cf0b21
QuadCorexRV <smooshman12345@gmail.com>
2025-01-18 09:26:11 +1100

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -40,7 +40,7 @@ object Versions {
     object Mod {
         const val mixin = "0.15.3+mixin.0.8.7"
         
-        const val minecraft = "1.21.3"
+        const val minecraft = "1.21.1"
         const val yarn = "$minecraft+build.2"
         const val fabricLoader = "0.16.7"
         
@@ -55,8 +55,8 @@ object Versions {
 //    }
     
     object Bukkit {
-        const val minecraft = "1.21.3"
-        const val paperBuild = "$minecraft-R0.1-20241101.150401-13"
+        const val minecraft = "1.21.1"
+        const val paperBuild = "$minecraft-R0.1-20240812.073803-10"
         const val paper = paperBuild
         const val paperLib = "1.0.8"
         const val reflectionRemapper = "0.1.1"
