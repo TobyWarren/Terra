2ef61ac3f686ee67421e36c4a7344d6d01511efe
QuadCorexRV <smooshman12345@gmail.com>
2025-03-20 09:52:42 +1100

    Fix addons Pt 6

---
+++ common/addons/library-image/src/main/java/com/dfsek/terra/addons/image/config/image/ImageTemplate.java
@@ -0,0 +1,33 @@
+package com.dfsek.terra.addons.image.config.image;
+
+import com.dfsek.tectonic.api.config.template.annotations.Value;
+import com.dfsek.tectonic.api.config.template.object.ObjectTemplate;
+
+import java.io.IOException;
+
+import com.dfsek.terra.addons.image.image.Image;
+import com.dfsek.terra.api.config.ConfigPack;
+import com.dfsek.terra.api.config.Loader;
+
+
+public class ImageTemplate implements ObjectTemplate<Image> {
+
+    private final Loader files;
+    private final ConfigPack pack;
+    @Value("path")
+    private String path;
+
+    public ImageTemplate(Loader files, ConfigPack pack) {
+        this.files = files;
+        this.pack = pack;
+    }
+
+    @Override
+    public Image get() {
+        try {
+            return ImageCache.load(path, pack, files);
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
